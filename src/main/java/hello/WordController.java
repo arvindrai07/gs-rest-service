bda01ef81d97ebb678780f7cd699c94bcb5b4f84
Arvind Rai <45809397+arvindrai07@users.noreply.github.com>
2018-12-12 20:14:31 +0530

    Adding method getWordStatus for request endpoint

--- src/main/java/hello/WordController.java
+++ src/main/java/hello/WordController.java
@@ -1,8 +1,19 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class WordController {
-    // TODO Implement the /words/{word} endpoint
+	
+	@Autowired
+	private WordService service;
+	
+	@GetMapping(value="/words/{word}")
+	public ResponseBean getWordStatus(@PathVariable String word){
+		return this.service.getWordStatus(word);
+	}
 }
